e04705e7ec6c3b6af1d21baf722e688d82aba301
chinmaymetil <chinmaymetil111@gmail.com>
2026-08-10 23:34:10 +0530

    first day

---
+++ task1_tony.py
@@ -0,0 +1,9 @@
+#name="Tony Stark" this is variable difine
+# age=53
+# height="1.85m"
+
+#print(type(age)) #variable cha datatype kalto hya mule
+# print(name, "is age:", age, "and height is:",height )  he print kele ahet variables
+
+hero=input("What is your superhero name?\n")  # he user kadun input ghetal ahe 
+print("my superhero name is :"+hero)  # eth varil input print kel ahe concatination krun.
