3555e05f93a699735dea7a250bf82acdfab66182
chinmaymetil <chinmaymetil111@gmail.com>
2026-08-12 15:44:25 +0530

    class practice

---
+++ YT practice/Class practice/secday.py
@@ -0,0 +1,20 @@
+'''import keyword  # hechya mule keyword kiti ani konte ahe te disat 
+print(keyword.kwlist)  
+
+
+a=10.77
+print(a)
+print(type(a))  #variable cha datatype show hot
+print(id(a))  #variable cha adress show kart'''
+
+
+#= single line cmt  
+''' 
+multiple line cmt 
+'''
+
+#complex datatype 
+x= 30+40j
+print(type(x))
+print(x.real) #30 real part
+print(x.imag) #40j imaginary part
