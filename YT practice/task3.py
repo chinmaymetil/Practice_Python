88b74f642fab2db99adc8b492e5d2488d068ffb6
chinmaymetil <chinmaymetil111@gmail.com>
2026-08-12 23:25:49 +0530

    home task

--- YT practice/task3.py
+++ YT practice/task3.py
@@ -7,8 +7,16 @@ print("Area of squaer =" ,side * side) # ethe side ** 2 he pn logic use karta aa
 a = float(input("Enter First No : "))
 b = float(input("Enter sec No : "))
 print("avarage =", (a+b)/2)
+
 '''total = a+b
 print("total =", total)
 # print avg =>
 print("avarage =", total/2)  # short form for this => print("avarage =", (a+b)/2)'''
+
+# Write a program to input 2 int numbers, a and b.
+# print True if a is greater than or equal to b. if not print false
+a=int(input("Enter 1st Number :"))
+b=int(input("Entew 2nd Number :"))
+
+print(a>=b)
 
