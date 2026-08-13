e3493d4f9fae47b97d6bb6ec237a338f38054df9
chinmaymetil <chinmaymetil111@gmail.com>
2026-08-13 15:42:09 +0530

    class

---
+++ Class Practice/day3.py
@@ -0,0 +1,32 @@
+#Arithmatic Opertors =>
+#adition 
+print(10+20)
+#subtraction
+print(10-20)
+#multipilication
+print(10*20)
+#Division
+print(10/3)
+#Floor division
+print(10//3)
+#moduls
+print(10%3)
+#Exponention square kadhanya sathi
+print(10**2)
+
+#Relational Operator
+print(2<5) 
+print(2>5) 
+print(2==2) 
+print(6>5) 
+print(3<5) 
+
+#logical Operator
+print(-3 and -2)
+print("" and "hello")
+print(0 or -1)
+print(" " or "5")
+print("0"or "bye")
+print("gm" or "gn")
+
+
