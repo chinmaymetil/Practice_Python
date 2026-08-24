5099ee6b28af7b76fd4b5a28c3b38481fe7892b7
chinmaymetil <chinmaymetil111@gmail.com>
2026-08-24 14:30:23 +0530

    tuple

--- Class Practice/day8.py
+++ Class Practice/day8.py
@@ -1,3 +1,45 @@
 # Python data Structure 
 
 # List 
+
+
+#Tuple
+
+t = (10, 20, 30, 40)
+print(t.index(30))
+
+# 2. len()
+t = (10, 20, 30, 40)
+print(len(t))
+
+# 3. count()
+t = (10, 20, 20, 30, 20)
+print(t.count(20))
+
+# 4. max()
+t = (10, 50, 20, 40)
+print(max(t))
+
+# 5. min()
+t = (10, 50, 20, 40)
+print(min(t))
+
+# 6. sum()
+t = (10, 20, 30, 40)
+print(sum(t))
+
+# 7. sorted()
+t = (40, 10, 30, 20)
+print(sorted(t))
+
+# 8. reversed()
+t = (10, 20, 30, 40)
+print(tuple(reversed(t)))
+
+
+# 9. slice
+t = (10, 20, 30, 40, 50)
+print(t[1:4])
+
+# 10. map()
+
