d241f79867c54a8520d97ec01855e70f3f543519
chinmaymetil <chinmaymetil111@gmail.com>
2026-08-31 23:32:08 +0530

    task

--- YT practice/file handling/file_handling.py
+++ YT practice/file handling/file_handling.py
@@ -37,17 +37,18 @@ def check_for_line():
 
 check_for_line()'''
 
-with open("demo.txt","r") as f:
-    data = f.read()
-    print(data)
 
-    num=""
-    for i in range(len(data)):
-        if(data[i]==","):
-            print(int(num))
-            num=""
-        else:
-            num+=data[i]
+count = 0
+with open("practice.txt","r") as f:
+    data = f.read()
+
+    num= data.split(",")
+    for val in num:
+        if(int(val) % 2==0):
+            print(int(val))
+            count += 1
+
+print("total even number :",count)
 
 
 
