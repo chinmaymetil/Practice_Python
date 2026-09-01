20682806829ba2d35ae48f6c70ef116c3d5450f2
chinmaymetil <chinmaymetil111@gmail.com>
2026-09-01 21:24:39 +0530

    modules

--- Class Practice/modules/main.py
+++ Class Practice/modules/main.py
@@ -1,3 +1,26 @@
-import test_module
+import test_module 
 name=input("What is Your Name ?\n")
-test_module.show("chinmay")
+test_module.show(name)
+
+# Variable Module
+a=test_module.person1["country"]
+print(a)
+
+# From-import 
+from calculation import addition 
+a=int(input("Enter 1st Number :"))
+b=int(input("Enter 2nd Number :"))
+print("Addition =",addition(a,b))
+#       OR
+from calculation import*  
+a=int(input("Enter 1st Number :"))
+b=int(input("Enter 2nd Number :"))
+print("Sub =",substraction(a,b))
+
+
+# Renaming a Module an use of alise(as)
+import calculation as cal
+a=int(input("Enter 1st Number :"))
+b=int(input("Enter 2nd Number :"))
+print("Multiplication =", cal.multiplication(a,b))
+
--- Class Practice/modules/test_module.py
+++ Class Practice/modules/test_module.py
@@ -3,3 +3,8 @@
 def show(name):
     print("I am",name)
 
+person1={
+     "name":"john",
+     "age":22,
+     "country":"India"
+}
