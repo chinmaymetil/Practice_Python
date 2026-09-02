98610d01b03205a9abb3fa5707aa2d9732dca669
chinmaymetil <chinmaymetil111@gmail.com>
2026-09-02 15:10:15 +0530

    class change

--- Class Practice/modules/main.py
+++ Class Practice/modules/main.py
@@ -1,3 +1,4 @@
+# It content multiple functions having your own logic 
 import test_module 
 name=input("What is Your Name ?\n")
 test_module.show(name)
@@ -12,6 +13,7 @@ a=int(input("Enter 1st Number :"))
 b=int(input("Enter 2nd Number :"))
 print("Addition =",addition(a,b))
 #       OR
+# ithe tya module madhale sagle function yetil * mule
 from calculation import*  
 a=int(input("Enter 1st Number :"))
 b=int(input("Enter 2nd Number :"))
@@ -23,4 +25,5 @@ import calculation as cal
 a=int(input("Enter 1st Number :"))
 b=int(input("Enter 2nd Number :"))
 print("Multiplication =", cal.multiplication(a,b))
+
 
--- Class Practice/modules/test_module.py
+++ Class Practice/modules/test_module.py
@@ -1,10 +1,10 @@
 # modules
-
 def show(name):
     print("I am",name)
 
+#variable module 
 person1={
-     "name":"john",
+     "name":"Chinmay",
      "age":22,
      "country":"India"
 }
