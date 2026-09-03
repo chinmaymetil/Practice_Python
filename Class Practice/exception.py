603229da78e095a1522e32a2d39cf94974562211
chinmaymetil <chinmaymetil111@gmail.com>
2026-09-03 22:58:06 +0530

    home task

--- Class Practice/exception.py
+++ Class Practice/exception.py
@@ -4,7 +4,7 @@ try:
     print(b)
 except:
     print("its an Example of Exception")
-    '''
+    
 
 try:
     with open("Not_file.txt","r") as f:
@@ -16,7 +16,20 @@ except IOError:
     print("error :- File io error")
 
 finally:
-    print("file operations attemped")
+    print("file operations attemped")'''
+
+
+# jar apan zero ne multiply karayala gelo tr error yete mhanun ha code  
+try:
+    a=int(input("Enter 1st number :"))
+    b=int(input("Enter 2nd number :"))
+    c=a/b
+    print("C=",c)
+except:
+    print("Error")
+print("Code executed successfully...")
+
+
 
 
 
