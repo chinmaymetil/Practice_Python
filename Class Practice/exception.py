331586c561a3c5c4864c93552d31181162785fc9
chinmaymetil <chinmaymetil111@gmail.com>
2026-09-04 16:00:33 +0530

    class

--- Class Practice/exception.py
+++ Class Practice/exception.py
@@ -16,10 +16,12 @@ except IOError:
     print("error :- File io error")
 
 finally:
+
     print("file operations attemped")'''
 
 
 # jar apan zero ne multiply karayala gelo tr error yete mhanun ha code  
+# Runtime Error
 try:
     a=int(input("Enter 1st number :"))
     b=int(input("Enter 2nd number :"))
@@ -28,6 +30,22 @@ try:
 except:
     print("Error")
 print("Code executed successfully...")
+
+#without Try Except ==>
+print("fortune")
+print(10/0)
+print("Cloud")
+
+
+# with try- except ==>
+
+print("Chinmay")
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print(10/2)
+print("Python")
+
 
 
 
